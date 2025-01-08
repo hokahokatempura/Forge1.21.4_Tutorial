53b628299affc61cca894e1af96c25333702ddfa
awa <asdasawagwegag+ab10@gmail.com>
2025-01-09 02:20:03 +0900

    Implement randomize command

--- src/main/java/com/hokahokatempura/tutorialmod/TutorialMod.java
+++ src/main/java/com/hokahokatempura/tutorialmod/TutorialMod.java
@@ -46,32 +46,52 @@ public class TutorialMod {
     // MOD内でコマンドを登録する場合は、以下のメソッドにコマンド登録処理を記述する
     @SubscribeEvent
     public void onRegisterLocalizeCommand(RegisterCommandsEvent event) {
-        // "putblock"コマンドを登録
-        LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("putblock")
-                // 引数を設定する ("ブロックを置く場所"という引数をVec3Argument.vec3()で受け取る)
-                .then(Commands.argument("ブロックを置く場所", Vec3Argument.vec3())
-                        .then(Commands.argument("X方向の長さ", IntegerArgumentType.integer())
-                                .then(Commands.argument("Y方向の長さ", IntegerArgumentType.integer())
-                                        .then(Commands.argument("Z方向の長さ", IntegerArgumentType.integer())
-                                                .executes(context -> {
-                                                    // コマンドが実行されると呼び出される処理
-                                                    TutorialCommand.putBlockCommand(context);
-                                                    return Command.SINGLE_SUCCESS;
-                                                })))));
-        event.getDispatcher().register(builder);
+        {
+            // "putblock"コマンドを登録
+            LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("putblock")
+                    // 引数を設定する ("ブロックを置く場所"という引数をVec3Argument.vec3()で受け取る)
+                    .then(Commands.argument("ブロックを置く場所", Vec3Argument.vec3())
+                            .then(Commands.argument("X方向の長さ", IntegerArgumentType.integer())
+                                    .then(Commands.argument("Y方向の長さ", IntegerArgumentType.integer())
+                                            .then(Commands.argument("Z方向の長さ", IntegerArgumentType.integer())
+                                                    .executes(context -> {
+                                                        // コマンドが実行されると呼び出される処理
+                                                        TutorialCommand.putBlockCommand(context);
+                                                        return Command.SINGLE_SUCCESS;
+                                                    })))));
+            event.getDispatcher().register(builder);
+        }
 
-        // "burning"コマンドを登録
-        LiteralArgumentBuilder<CommandSourceStack> builder2 = Commands.literal("burning")
-                // 引数を設定する ("ブロックを置く場所"という引数をVec3Argument.vec3()で受け取る)
-                .then(Commands.argument("中心の座標", Vec3Argument.vec3())
-                        .then(Commands.argument("X方向の長さ", IntegerArgumentType.integer())
-                                .then(Commands.argument("Y方向の長さ", IntegerArgumentType.integer())
-                                        .then(Commands.argument("Z方向の長さ", IntegerArgumentType.integer())
-                                                .executes(context -> {
-                                                    // コマンドが実行されると呼び出される処理
-                                                    TutorialCommand.burningCommand(context);
-                                                    return Command.SINGLE_SUCCESS;
-                                                })))));
-        event.getDispatcher().register(builder2);
+        {
+            // "burning"コマンドを登録
+            LiteralArgumentBuilder<CommandSourceStack> builder2 = Commands.literal("burning")
+                    // 引数を設定する ("ブロックを置く場所"という引数をVec3Argument.vec3()で受け取る)
+                    .then(Commands.argument("中心の座標", Vec3Argument.vec3())
+                            .then(Commands.argument("X方向の長さ", IntegerArgumentType.integer())
+                                    .then(Commands.argument("Y方向の長さ", IntegerArgumentType.integer())
+                                            .then(Commands.argument("Z方向の長さ", IntegerArgumentType.integer())
+                                                    .executes(context -> {
+                                                        // コマンドが実行されると呼び出される処理
+                                                        TutorialCommand.burningCommand(context);
+                                                        return Command.SINGLE_SUCCESS;
+                                                    })))));
+            event.getDispatcher().register(builder2);
+        }
+
+        {
+            // "randomize"コマンドを登録
+            LiteralArgumentBuilder<CommandSourceStack> builder3 = Commands.literal("randomize")
+                    // 引数を設定する ("ブロックを置く場所"という引数をVec3Argument.vec3()で受け取る)
+                    .then(Commands.argument("中心の座標", Vec3Argument.vec3())
+                            .then(Commands.argument("X方向の長さ", IntegerArgumentType.integer())
+                                    .then(Commands.argument("Y方向の長さ", IntegerArgumentType.integer())
+                                            .then(Commands.argument("Z方向の長さ", IntegerArgumentType.integer())
+                                                    .executes(context -> {
+                                                        // コマンドが実行されると呼び出される処理
+                                                        TutorialCommand.randomizeCommand(context);
+                                                        return Command.SINGLE_SUCCESS;
+                                                    })))));
+            event.getDispatcher().register(builder3);
+        }
     }
 }
--- src/main/java/com/hokahokatempura/tutorialmod/commands/TutorialCommand.java
+++ src/main/java/com/hokahokatempura/tutorialmod/commands/TutorialCommand.java
@@ -32,6 +32,7 @@ public class TutorialCommand {
         }
     }
 
+    // "burning"コマンドが実行されたときの処理
     public static void burningCommand(CommandContext<CommandSourceStack> context) {
         var pos = Vec3Argument.getVec3(context, "中心の座標");
         var source = context.getSource();
@@ -55,6 +56,36 @@ public class TutorialCommand {
                             // 火を設置する
                             source.getLevel().setBlock(targetPos, Blocks.FIRE.defaultBlockState(), 3);
                         }
+                    }
+                }
+            }
+        }
+    }
+
+    // "randomize"コマンドが実行されたときの処理
+    public static void randomizeCommand(CommandContext<CommandSourceStack> context) {
+        var pos = Vec3Argument.getVec3(context, "中心の座標");
+        var source = context.getSource();
+        var blockpos = new BlockPos((int) pos.x, (int) pos.y, (int) pos.z);
+
+        var numX = context.getArgument("X方向の長さ", Integer.class);
+        var numY = context.getArgument("Y方向の長さ", Integer.class);
+        var numZ = context.getArgument("Z方向の長さ", Integer.class);
+
+        // X方向に-numX ~ numX、Y方向に-numY ~ numY、Z方向に-numZ ~ numZの範囲のブロックをランダムに書き換える
+        for (int x = -numX; x < numX; x++) {
+            for (int y = -numY; y < numY; y++) {
+                for (int z = -numZ; z < numZ; z++) {
+                    var targetPos = blockpos.offset(x, y, z);
+                    // 対象のブロックが空気ではなかった場合のみ処理を行う
+                    if (source.getLevel().getBlockState(targetPos) != Blocks.AIR.defaultBlockState())
+                    {
+                        // ブロックリストからランダムなブロックを取得
+                        var randomIndex = (int)(Math.random() * BlockList.blocks.length);
+                        var randomBlock = BlockList.blocks[randomIndex];
+
+                        // 対象のブロックをランダムなブロックに書き換える
+                        source.getLevel().setBlock(targetPos, randomBlock.defaultBlockState(), 3);
                     }
                 }
             }
