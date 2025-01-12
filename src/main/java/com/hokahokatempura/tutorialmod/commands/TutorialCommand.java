4a13bfcc29f0dc9e924da7c490d765bcd6851bbb
awa <asdasawagwegag+ab10@gmail.com>
2025-01-13 02:54:29 +0900

    set level as local variable

--- src/main/java/com/hokahokatempura/tutorialmod/commands/TutorialCommand.java
+++ src/main/java/com/hokahokatempura/tutorialmod/commands/TutorialCommand.java
@@ -15,17 +15,18 @@ public class TutorialCommand {
         var pos = Vec3Argument.getVec3(context, "ブロックを置く場所");
         var source = context.getSource();
         var blockpos = new BlockPos((int) pos.x, (int) pos.y, (int) pos.z);
+        var level = source.getLevel();
 
         var numX = context.getArgument("X方向の長さ", Integer.class);
         var numY = context.getArgument("Y方向の長さ", Integer.class);
         var numZ = context.getArgument("Z方向の長さ", Integer.class);
 
-        // X方向にnumX、Y方向にnumY、Z方向にnumZの長さのダイヤモンドブロックを設置
+        // X方向にnumX、Y方向にnumY、Z方向にnumZ回だけ処理を繰り返す
         for (int x = 0; x < numX; x++) {
             for (int y = 0; y < numY; y++) {
                 for (int z = 0; z < numZ; z++) {
-                    // posの位置にダイヤモンドブロックを設置
-                    source.getLevel().setBlock(blockpos.offset(x, y, z), Blocks.DIAMOND_BLOCK.defaultBlockState(), 3);
+                    // blockposの位置にダイヤモンドブロックを設置
+                    level.setBlock(blockpos.offset(x, y, z), Blocks.DIAMOND_BLOCK.defaultBlockState(), 3);
                 }
             }
         }
